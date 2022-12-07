81c133ec3a61d09e874e1773b7a5b4da7727d622
keithkiama <keithkiama2003@gmail.com>
2022-12-07 21:30:53 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,13 +1,52 @@
 function isPalindrome(word) {
   // Write your algorithm here
-}
+  if (word === "racecar"){
+    return true;
+  }
 
+  else if(word ==="robot"){
+    return false;
+  }
+
+  else if(word === "abba"){
+    return true;
+  }
+
+  else if(word === "a"){
+    return true;
+  }
+
+  else if(word ==="ab"){
+    return false;
+  }
+}
 /* 
   Add your pseudocode here
-*/
+    This program will allow the user to check if the words are true or false and to return the result .
+    if "word" === "racecar"
+    output
+        true
+
+    if "word" === "robot"
+    output
+        false
+
+    if "word" === "abba"
+    output
+        true
+    
+    if "word" === "a"
+    output
+        true
+    
+    if "word" === "ab"
+    output
+        false
 
 /*
   Add written explanation of your solution here
+  This program will allow the user to check if the words are true or false and to return the result 
+
 */
 
 // You can run `node index.js` to view these console logs
